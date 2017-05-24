6d58cfff872f163259c7baa3b736cc941f020e73
ingriarce4 <ingridarce4@gmail.com>
2017-05-24 05:51:12 -0400

    eventos

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+/* Defino el tablero
+var tablero = [
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0]
+]
+//Ingreso el auto en la posicion 3,5
+tablero[3][5] = "A";
+
+//Obtengo el div en donde se dibujará el tablero
+var divTablero = document.getElementById("tablero");
+
+//Variables temporales
+var fila, casilla;
+
+//Recorro el arreglo para dibujar el tablero
+for(var i = 0; i<tablero.length; i++){
+  fila = document.createElement("div");
+  fila.classList.add("fila");
+  for(var j = 0; j<tablero[i].length; j++){
+    casilla = document.createElement("div");
+    casilla.innerHTML = tablero[i][j];
+    casilla.classList.add("casilla")
+    fila.appendChild(casilla);
+  }
+  divTablero.appendChild(fila);
+}  */
+
+//evento onclick
+
+var tablero = function(){
+
+};
+
+//agregamos el tablero
+(function(){
+  var boton = document.getElementById("start");
+  boton.addEventListener("click",tablero);
+
+  }
+
+}())
